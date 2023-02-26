194d5640db12a98f3d2c87b44b4421853ae9ee5f
Syed Amaan <75304633+Ak-407@users.noreply.github.com>
2023-02-26 21:02:46 +0530

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
   return (<>
 
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="#">Writing Wonder</a>
+  <a class="navbar-brand" href="#">Cloud Wonder</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
